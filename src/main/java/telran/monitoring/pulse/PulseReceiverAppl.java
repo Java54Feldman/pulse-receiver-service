87dcd96354f176cdd5cb18516e6937ede131c05a
Pavel Feldman <feldmanpk@gmail.com>
2024-10-22 22:44:31 +0300

    Migration SDK-1 -> SDK-2 for connection and put items to DynamoDB

--- src/main/java/telran/monitoring/pulse/PulseReceiverAppl.java
+++ src/main/java/telran/monitoring/pulse/PulseReceiverAppl.java
@@ -1,22 +1,30 @@
 package telran.monitoring.pulse;
-import java.net.*;
-import java.util.Arrays;
 
-import com.amazonaws.regions.Regions;
-import com.amazonaws.services.dynamodbv2.*;
-import com.amazonaws.services.dynamodbv2.document.*;
-import com.amazonaws.services.dynamodbv2.document.spec.PutItemSpec;
+import java.net.*;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import software.amazon.awssdk.auth.credentials.*;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.dynamodb.model.*;
 
 public class PulseReceiverAppl {
 	private static final int PORT = 5000;
 	private static final int MAX_BUFFER_SIZE = 1500;
+	private static final String AWS_ACCESS_KEY_ID = "AWS_ACCESS_KEY_ID";
+	private static final String AWS_SECRET_KEY = "AWS_SECRET_KEY";
+	private static final String TABLE_NAME = "pulse_values";
 	static DatagramSocket socket;
-	static AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
-			.withRegion(Regions.US_EAST_1)
+	static DynamoDbClient client = DynamoDbClient.builder()
+			.region(Region.US_EAST_1)
+            .credentialsProvider(StaticCredentialsProvider.create(
+                    AwsBasicCredentials.create(
+                        System.getenv(AWS_ACCESS_KEY_ID),
+                        System.getenv(AWS_SECRET_KEY)
+                    )))
 			.build();
-	static DynamoDB dynamo = new DynamoDB(client);
-	static Table table = dynamo.getTable("pulse_values");
-	
+
 	public static void main(String[] args) throws Exception {
 		socket = new DatagramSocket(PORT);
 		byte[] buffer = new byte[MAX_BUFFER_SIZE];
@@ -26,10 +34,23 @@ public class PulseReceiverAppl {
 			processReceivedData(buffer, packet);
 		}
 	}
+
 	private static void processReceivedData(byte[] buffer, DatagramPacket packet) {
 		String json = new String(Arrays.copyOf(buffer, packet.getLength()));
 		System.out.println(json);
-		table.putItem(new PutItemSpec().withItem(Item.fromJSON(json)));
+
+		Map<String, AttributeValue> item = Arrays.stream(json.replaceAll("[{}\"\\s]", "").split(","))
+				.map(pair -> pair.split(":")).filter(keyValue -> keyValue.length == 2)
+				.collect(Collectors.toMap(
+						keyValue -> keyValue[0], 
+						keyValue -> AttributeValue.builder().n(keyValue[1]).build()));
+
+		PutItemRequest request = PutItemRequest.builder()
+				.tableName(TABLE_NAME)
+				.item(item)
+				.build();
+
+		client.putItem(request);
 	}
 
 }
