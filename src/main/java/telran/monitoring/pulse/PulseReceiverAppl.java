9690b5443d414421f59699983797881429cd0601
Pavel Feldman <feldmanpk@gmail.com>
2024-10-22 20:30:04 +0300

    CW #62 connect to DynamoDB

--- src/main/java/telran/monitoring/pulse/PulseReceiverAppl.java
+++ src/main/java/telran/monitoring/pulse/PulseReceiverAppl.java
@@ -2,10 +2,21 @@ package telran.monitoring.pulse;
 import java.net.*;
 import java.util.Arrays;
 
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.*;
+import com.amazonaws.services.dynamodbv2.document.*;
+import com.amazonaws.services.dynamodbv2.document.spec.PutItemSpec;
+
 public class PulseReceiverAppl {
 	private static final int PORT = 5000;
 	private static final int MAX_BUFFER_SIZE = 1500;
 	static DatagramSocket socket;
+	static AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
+			.withRegion(Regions.US_EAST_1)
+			.build();
+	static DynamoDB dynamo = new DynamoDB(client);
+	static Table table = dynamo.getTable("pulse_values");
+	
 	public static void main(String[] args) throws Exception {
 		socket = new DatagramSocket(PORT);
 		byte[] buffer = new byte[MAX_BUFFER_SIZE];
@@ -18,6 +29,7 @@ public class PulseReceiverAppl {
 	private static void processReceivedData(byte[] buffer, DatagramPacket packet) {
 		String json = new String(Arrays.copyOf(buffer, packet.getLength()));
 		System.out.println(json);
+		table.putItem(new PutItemSpec().withItem(Item.fromJSON(json)));
 	}
 
 }
